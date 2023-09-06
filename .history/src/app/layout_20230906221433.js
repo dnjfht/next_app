c7f018fb44536aa6f58f1b0dbe8a8026fbd04d4a
Yoo Seungmin <dnjfht@gmail.com>
2023-09-07 00:12:09 +0900

    샙픋 앱 세탁

--- .history/src/app/layout_20230906221433.js
+++ .history/src/app/layout_20230906221433.js
@@ -1,4 +1,7 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -7,8 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
